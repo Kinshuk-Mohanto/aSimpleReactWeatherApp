94f4a61b19c172d939f0a66f8aa50534a4babadb
Kinshuk-Mohanto <69083863+Kinshuk-Mohanto@users.noreply.github.com>
2020-08-17 17:40:14 +0600

    Add files via upload

---
+++ build/precache-manifest.c60cb438e47b9d71fa3d8ed63842e4f7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da44f900d5d69eccb1a044dc8b08489",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a5ad697119112bc67b4",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "28c90f50d60987b69ca7",
+    "url": "/static/css/main.3526ac36.chunk.css"
+  },
+  {
+    "revision": "6a5ad697119112bc67b4",
+    "url": "/static/js/2.d3bed85a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d3bed85a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c90f50d60987b69ca7",
+    "url": "/static/js/main.361a7acd.chunk.js"
+  },
+  {
+    "revision": "baf0f03f1adcb4485ad8",
+    "url": "/static/js/runtime-main.c64402b6.js"
+  },
+  {
+    "revision": "6223b5ab6e6fc72d9f2b91f35fe54aac",
+    "url": "/static/media/cold-bg.6223b5ab.png"
+  },
+  {
+    "revision": "43ab2e5b127a180861a97032fcbc9b67",
+    "url": "/static/media/warm-bg.43ab2e5b.jfif"
+  }
+]);
